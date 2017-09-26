a1532e339e05567324c81d47bff3a374fd05fee5
huangchenliang <358901886@qq.com>
2017-09-26 16:20:07 +0800

    1

---
+++ zipkin-server/src/main/java/com/hchl/ZipkinApplication.java
@@ -0,0 +1,23 @@
+package com.hchl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * 类 编 号：
+ * 类 名 称：
+ * 内容摘要：
+ * 创建日期：2017/9/26
+ * 编码作者：hcl
+ */
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
